85e5fe0a9d0c84f6159c2d485f6fa23f5bf7d030
Denis Kel <asket2013@ya.ru>
2022-12-01 13:52:09 +1000

    Second

--- main.py
+++ main.py
@@ -22,48 +22,48 @@ def create_db(connect):
 
 
 def add_client(connect, first_name, last_name, email, phone=None):
-    with connect.cursor() as cur:
-        if phone is None:
-            cur.execute("""
-            INSERT INTO clients(first_name, last_name, email) VALUES(%s, %s, %s);
-            """, (first_name, last_name, email))
-            connect.commit()
-        else:
-            cur.execute("""
-            INSERT INTO clients(first_name, last_name, email, phone) VALUES(%s, %s, %s, %s) RETURNING id;
-            """, (first_name, last_name, email, phone))
-            cur.execute("""
-            INSERT INTO phones(phone, client_id) VALUES(%s, %s);
-            """, (phone, cur.fetchone()[0]))
-            # connect.commit()
+    # with connect.cursor() as cur:
+    #     if phone is None:
+    #         cur.execute("""
+    #         INSERT INTO clients(first_name, last_name, email) VALUES(%s, %s, %s);
+    #         """, (first_name, last_name, email))
+    #         connect.commit()
+    #     else:
+    #         cur.execute("""
+    #         INSERT INTO clients(first_name, last_name, email, phone) VALUES(%s, %s, %s, %s) RETURNING id;
+    #         """, (first_name, last_name, email, phone))
+    #         cur.execute("""
+    #         INSERT INTO phones(phone, client_id) VALUES(%s, %s);
+    #         """, (phone, cur.fetchone()[0]))
+    #         # connect.commit()
 
 
-def count_phone(cursor, client_id):
-    cursor.execute("""
-    SELECT COUNT(phone) FROM phones WHERE client_id=%s;
-    """, str(client_id))
-    return cursor.fetchone()[0]
+# def count_phone(cursor, client_id):
+    # cursor.execute("""
+    # SELECT COUNT(phone) FROM phones WHERE client_id=%s;
+    # """, str(client_id))
+    # return cursor.fetchone()[0]
 
 
 def add_phone(connect, phone, client_id):
-    with connect.cursor() as cur:
-        if count_phone(cur, client_id) >= 1:
-            cur.execute("""
-            INSERT INTO phones(phone, client_id) VALUES(%s, %s);
-            """, (phone, client_id))
-            connect.commit()
-        else:
-            cur.execute("""
-            INSERT INTO phones(phone, client_id) VALUES(%s, %s);
-            """, (phone, client_id))
-            change_client(connect, client_id=client_id, phone=phone)
+    # with connect.cursor() as cur:
+    #     if count_phone(cur, client_id) >= 1:
+    #         cur.execute("""
+    #         INSERT INTO phones(phone, client_id) VALUES(%s, %s);
+    #         """, (phone, client_id))
+    #         connect.commit()
+    #     else:
+    #         cur.execute("""
+    #         INSERT INTO phones(phone, client_id) VALUES(%s, %s);
+    #         """, (phone, client_id))
+    #         change_client(connect, client_id=client_id, phone=phone)
 
 
-def search_phone(cursor, client_id):
-    cursor.execute("""
-    SELECT phone FROM clients WHERE id=%s;
-    """, str(client_id))
-    return cursor.fetchone()[0]
+# def search_phone(cursor, client_id):
+#     cursor.execute("""
+#     SELECT phone FROM clients WHERE id=%s;
+#     """, str(client_id))
+#     return cursor.fetchone()[0]
 
 
 def change_client(connect, client_id, first_name=None, last_name=None, email=None, phone=None):
@@ -83,26 +83,26 @@ def change_client(connect, client_id, first_name=None, last_name=None, email=Non
             UPDATE clients SET email=%s WHERE id=%s;
             """, (email, client_id))
             connect.commit()
-        if phone is not None:
-            if search_phone(cur, client_id) is not None:
-                cur.execute("""
-                UPDATE phones SET phone=%s WHERE phone=%s;
-                """, (phone, search_phone(cur, client_id)))
-            cur.execute("""
-            UPDATE clients SET phone=%s WHERE id=%s;
-            """, (phone, client_id))
-            connect.commit()
+        # if phone is not None:
+        #     if search_phone(cur, client_id) is not None:
+        #         cur.execute("""
+        #         UPDATE phones SET phone=%s WHERE phone=%s;
+        #         """, (phone, search_phone(cur, client_id)))
+        #     cur.execute("""
+        #     UPDATE clients SET phone=%s WHERE id=%s;
+        #     """, (phone, client_id))
+        #     connect.commit()
 
 
 def delete_phone(connect, client_id, phone):
-    with connect.cursor() as cur:
-        cur.execute("""
-        DELETE FROM phones WHERE phone=%s;
-        """, (phone,))
-
-        cur.execute("""
-        DELETE phone FROM clients WHERE id=%s;
-        """, (client_id,))
+    # with connect.cursor() as cur:
+    #     cur.execute("""
+    #     DELETE FROM phones WHERE phone=%s;
+    #     """, (phone,))
+    #
+    #     cur.execute("""
+    #     DELETE phone FROM clients WHERE id=%s;
+    #     """, (client_id,))
 
 
 
@@ -120,14 +120,14 @@ with psycopg2.connect(database="clients_db", user="postgres", password="123") as
     add_client(conn, '2Rob2', '2Wholes2', '2rob@rob.com', '79999999999')
     add_client(conn, '3Rob3', '3Wholes3', '3rob@rob.com')
     add_client(conn, '4Rob4', '4Wholes4', '4rob@rob.com')
-    add_phone(conn, client_id=3, phone='79991111111')
-    add_phone(conn, client_id=3, phone='79992222222')
-    add_phone(conn, client_id=3, phone='79993333333')
-    add_phone(conn, client_id=4, phone='79994444444')
-    change_client(conn, client_id=1, first_name='1Bob1')
-    change_client(conn, client_id=3, phone='77777777777')
-    delete_phone(conn, client_id=4, phone='79994444444')
-    delete_phone(conn, client_id=3, phone='79992222222')
+    # add_phone(conn, client_id=3, phone='79991111111')
+    # add_phone(conn, client_id=3, phone='79992222222')
+    # add_phone(conn, client_id=3, phone='79993333333')
+    # add_phone(conn, client_id=4, phone='79994444444')
+    # change_client(conn, client_id=1, first_name='1Bob1')
+    # change_client(conn, client_id=3, phone='77777777777')
+    # delete_phone(conn, client_id=4, phone='79994444444')
+    # delete_phone(conn, client_id=3, phone='79992222222')
 
 
 conn.close()
